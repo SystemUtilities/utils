612aa93878cf71377c3535e39fd02d4fb124ff89
Anit Shrestha Manandhar <codeanit@gmail.com>
2021-08-12 17:20:42 +0545

    Change Containerd and K8s

--- containerd-install.sh
+++ containerd-install.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 set -e
-sudo modprobe overlay
-sudo modprobe br_netfilter
 sudo apt-get update -y
 sudo apt-get install -y containerd
+sudo modprobe overlay
+sudo modprobe br_netfilter
 sudo cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
 overlay
 br_netfilter
 EOF
+sudo systemctl daemon-reload
 sudo mkdir -p /etc/containerd
 sudo containerd config default > /etc/containerd/config.toml
 sudo sed -i -e 's/            SystemdCgroup = false/            SystemdCgroup = true/' /etc/containerd/config.toml
