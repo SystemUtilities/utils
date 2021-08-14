3d7caf4e77d4a1b0c80899b1c1579ffb352375a5
Anit Shrestha Manandhar <codeanit@gmail.com>
2021-08-14 19:34:10 +0545

    Add containerd config

--- containerd-install.sh
+++ containerd-install.sh
@@ -2,13 +2,19 @@
 set -e
 sudo apt-get update -y
 sudo apt-get install -y containerd
-sudo modprobe overlay
-sudo modprobe br_netfilter
 sudo cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
 overlay
 br_netfilter
 EOF
-sudo systemctl daemon-reload
+sudo modprobe overlay
+sudo modprobe br_netfilter
+# Setup required sysctl params, these persist across reboots.
+sudo cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+sudo sysctl --system
 sudo mkdir -p /etc/containerd
 sudo containerd config default | sudo tee /etc/containerd/config.toml
 sudo sed -i -e 's/            SystemdCgroup = false/            SystemdCgroup = true/' /etc/containerd/config.toml
